fe7caff1b912547e10579b2eeb12019cbfba72c8
Josh Long <josh@joshlong.com>
2020-03-13 02:17:49 -0700

    twitter ftw

--- src/main/kotlin/experiment/twitter.kt
+++ src/main/kotlin/experiment/twitter.kt
@@ -6,6 +6,8 @@ import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
 import org.springframework.context.ApplicationListener
 import org.springframework.context.annotation.Bean
+import org.springframework.core.env.Environment
+import org.springframework.core.env.get
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpRequest
 import org.springframework.http.MediaType
@@ -22,19 +24,18 @@ import java.util.concurrent.atomic.AtomicReference
 @SpringBootApplication
 class TwitterClientApplication {
 
-	private val apiKey = ""
-	private val apiKeySecret = ""
 
 	@Bean
-	fun restTemplate() = RestTemplate()
+	fun authenticatedRestTemplate(env: Environment) = RestTemplate()
 			.apply {
+				val apiKey = env["TWITTER_ORGANIZER_CLIENT_KEY"]!!
+				val apiKeySecret = env["TWITTER_ORGANIZER_CLIENT_KEY_SECRET"]!!
 				interceptors.add(BearerTokenInterceptor(apiKey, apiKeySecret))
 			}
 
 	@Bean
-	fun twitterClient() = TwitterClient(restTemplate())
+	fun twitterClient(rt: RestTemplate) = TwitterClient(rt)
 }
-
 
 
 //https://developer.twitter.com/en/docs/basics/authentication/oauth-2-0/bearer-tokens
@@ -52,34 +53,39 @@ class TwitterClient(
 
 	override fun onApplicationEvent(event: ApplicationReadyEvent) {
 		timelineFor("starbuxman")
+		timelineFor("springrod")
 	}
 
 }
 
 fun main() {
-
 	runApplication<TwitterClientApplication>()
 }
 
 
-class BearerTokenInterceptor(private val key: String, private val secret: String)
-	: ClientHttpRequestInterceptor {
+class BearerTokenInterceptor(
+		private val key: String, private val secret: String) : ClientHttpRequestInterceptor {
 
 	private val accessTokenReference = AtomicReference<String>()
+
+	// this is used only to obtain the token. it should not be the
+	// same RT instance as the one being used to make OAuth-authenticated calls.
 	private val accessTokenTemplate = RestTemplate()
 
+	// todo figure out how rate limiting and timeouts interacts with this test.
+	//      the tokens should be refreshed more frequently than the lifecycle of this application.
 	private fun shouldRefreshToken() = this.accessTokenReference.get() == null
 
 	override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution): ClientHttpResponse {
 		val token = obtainToken(accessTokenTemplate, key, secret)
-		println( "obtained access token: ${token}")
-		request.headers.add("Authorization", "Bearer $token")
+		request.headers.add(HttpHeaders.AUTHORIZATION, "Bearer $token")
 		return execution.execute(request, body)
 	}
 
 	private fun obtainToken(
 			restTemplate: RestTemplate,
-			key: String, secret: String): String {
+			key: String,
+			secret: String): String {
 
 		if (!shouldRefreshToken()) {
 			return this.accessTokenReference.get()
@@ -88,8 +94,7 @@ class BearerTokenInterceptor(private val key: String, private val secret: String
 		val httpHeaders = HttpHeaders(LinkedMultiValueMap())
 				.apply {
 					contentType = MediaType.APPLICATION_JSON
-					val encodedCredentials =
-							HttpHeaders.encodeBasicAuth(key, secret, Charset.defaultCharset())
+					val encodedCredentials = HttpHeaders.encodeBasicAuth(key, secret, Charset.defaultCharset())
 					if (!containsKey(HttpHeaders.AUTHORIZATION)) {
 						setBasicAuth(encodedCredentials);
 					}
@@ -98,7 +103,9 @@ class BearerTokenInterceptor(private val key: String, private val secret: String
 		val request: RequestEntity<Void> = RequestEntity.post(URI.create(uri)).headers(httpHeaders).build()
 		val responseEntityJsonNode = restTemplate.postForEntity(uri, request, JsonNode::class.java)
 		val jsonNode = responseEntityJsonNode.body!!
-		return jsonNode["access_token"].textValue()
+		val at = jsonNode["access_token"].textValue()
+		this.accessTokenReference.set(at)
+		return at
 	}
 
 }
