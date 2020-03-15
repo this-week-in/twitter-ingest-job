f03b7b5745de4416f656d3c9a95a96de1a4a3efd
Josh Long <josh@joshlong.com>
2020-03-15 04:33:37 -0700

    updated to java 11

--- src/test/kotlin/com/joshlong/twitter/api/TwitterClientIntegrationTests.kt
+++ src/test/kotlin/com/joshlong/twitter/api/TwitterClientIntegrationTests.kt
@@ -2,8 +2,10 @@ package com.joshlong.twitter.api
 
 import org.junit.Assert
 import org.junit.Test
+import org.junit.jupiter.api.Disabled
 import org.springframework.web.client.RestTemplate
 
+@Disabled
 class TwitterClientIntegrationTests {
 
 	private val authenticatedRestTemplate =
@@ -18,7 +20,7 @@ class TwitterClientIntegrationTests {
 
 	@Test
 	fun `should be able to get tweets from other users`() {
-		val username = "springrod"
+		val username = "SpringCentral"
 		val timeline = this.twitterClient.getUserTimeline(username)
 		Assert.assertTrue(timeline.isNotEmpty())
 		timeline.forEach {
