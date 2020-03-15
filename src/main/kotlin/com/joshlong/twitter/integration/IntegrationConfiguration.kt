405608b76ed54f2db78f2b1b29c79220e46a6e15
Josh Long <josh@joshlong.com>
2020-03-15 04:22:12 -0700

    updated to java 11

--- src/main/kotlin/com/joshlong/twitter/integration/IntegrationConfiguration.kt
+++ src/main/kotlin/com/joshlong/twitter/integration/IntegrationConfiguration.kt
@@ -1,5 +1,6 @@
 package com.joshlong.twitter.integration
 
+import com.joshlong.twitter.IngestTwitterProperties
 import com.joshlong.twitter.api.BaseTwitterClient
 import com.joshlong.twitter.api.BearerTokenInterceptor
 import com.joshlong.twitter.api.HttpTwitterClient
@@ -9,8 +10,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.context.annotation.Scope
-import org.springframework.core.env.Environment
-import org.springframework.core.env.get
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.integration.context.IntegrationContextUtils
@@ -21,27 +20,36 @@ import pinboard.PinboardClient
 import java.util.concurrent.Executor
 
 @Configuration
-class IntegrationConfiguration {
+class IntegrationConfiguration(private val ingestTwitterProperties: IngestTwitterProperties) {
 
 	private val profileToTags: Map<String, List<String>> = mapOf(
 			"springcentral" to listOf("spring", "pivotal", "twis", "ingest"),
 			"pivotal" to listOf("pivotal", "ingest"),
+			"vmware" to listOf("vmware", "ingest"),
+			"JavaAtMicrosoft" to listOf("vmware", "twis", "ingest", "microsoft", "java"),
+			"vmwaretanzu" to listOf("vmware", "ingest", "twis"),
+			"rabbitmq" to listOf("rabbitmq", "twis", "ingest"),
+			"confluentInc" to listOf("kafka", "twis", "ingest"),
 			"cloudfoundry" to listOf("cloudfoundry", "twis", "cloud", "ingest"),
-//			"dzone" to listOf("programming", "ingest"),
 			"springrod" to listOf("spring", "atomist", "ingest"),
 			"rob_winch" to listOf("spring", "spring-security", "security", "ingest"),
 			"springframework" to listOf("spring", "twis", "ingest"),
 			"springboot" to listOf("spring", "spring-boot", "twis", "ingest"),
 			"springcloud" to listOf("spring", "spring-cloud", "spring-boot", "twis", "ingest"),
-			"springsecurity" to listOf("spring", "spring-security", "security", "ingest"),
+			"springsecurity" to listOf("spring", "twis", "spring-security", "security", "ingest"),
 			"springdata" to listOf("spring", "spring-data", "twis", "ingest"),
-			"java" to listOf("java", "ingest"),
+			"java" to listOf("java", "twis", "ingest"),
 			"wattersjames" to listOf("cloudfoundry", "pivotal", "spring", "ingest"),
 			"olivergierke" to listOf("spring", "ingest", "spring-data", "ingest"),
 			"phillip_webb" to listOf("spring", "ingest", "spring-boot", "ingest"),
 			"david_syer" to listOf("spring", "ingest", "spring-boot", "spring-cloud", "ingest"),
 			"spencerbgibb" to listOf("spring", "ingest", "spring-boot", "spring-cloud", "ingest")
 	)
+
+	private val authenticatedRestTemplate = RestTemplate().apply {
+		val bearerTokenInterceptor = BearerTokenInterceptor(ingestTwitterProperties.consumerKey, ingestTwitterProperties.consumerSecret)
+		interceptors.add(bearerTokenInterceptor)
+	}
 
 	@Bean
 	@Profile("cloud")
@@ -54,15 +62,8 @@ class IntegrationConfiguration {
 	@Bean(IntegrationContextUtils.METADATA_STORE_BEAN_NAME)
 	fun redisMetadataStore(stringRedisTemplate: StringRedisTemplate) = RedisMetadataStore(stringRedisTemplate)
 
-	fun authenticatedRestTemplate(env: Environment) = RestTemplate()
-			.apply {
-				val apiKey = env["TWITTER_ORGANIZER_CLIENT_KEY"]!!
-				val apiKeySecret = env["TWITTER_ORGANIZER_CLIENT_KEY_SECRET"]!!
-				interceptors.add(BearerTokenInterceptor(apiKey, apiKeySecret))
-			}
-
 	@Bean
-	fun twitterClient(env: Environment): BaseTwitterClient = HttpTwitterClient(authenticatedRestTemplate(env))
+	fun twitterClient(ingestTwitterProperties: IngestTwitterProperties): BaseTwitterClient = HttpTwitterClient(this.authenticatedRestTemplate)
 
 	@Bean
 	fun stringRedisTemplate(rcf: RedisConnectionFactory) = StringRedisTemplate(rcf)
