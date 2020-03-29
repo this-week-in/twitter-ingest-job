4c3a0c5758bd81cc1350bf0b0df6fcacf48768fe
Josh Long <josh@joshlong.com>
2020-03-29 01:53:51 -0700

    polish

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -16,12 +16,12 @@ cf bs ${APP_NAME} ${SCHEDULER_SERVICE_NAME}
 cf s | grep ${REDIS_NAME} || cf cs rediscloud 100mb ${REDIS_NAME}
 cf bs ${APP_NAME} ${REDIS_NAME}
 
-cf set-env ${APP_NAME} PINBOARD_TOKEN ${PINBOARD_TOKEN}
+cf set-env ${APP_NAME} PINBOARD_TOKEN "${PINBOARD_TOKEN}"
 cf set-env ${APP_NAME} TWITTER_TWI_CLIENT_KEY ${TWITTER_TWI_CLIENT_KEY}
 cf set-env ${APP_NAME} TWITTER_TWI_CLIENT_KEY_SECRET ${TWITTER_TWI_CLIENT_KEY_SECRET}
 cf set-env ${APP_NAME} JBP_CONFIG_OPEN_JDK_JRE '{ jre: { version: 11.+}}'
 
-cf restage ${APP_NAME}
+cf restart ${APP_NAME}
 
 cf jobs  | grep $JOB_NAME && cf delete-job -f ${JOB_NAME}
 cf create-job ${APP_NAME} ${JOB_NAME} ".java-buildpack/open_jdk_jre/bin/java org.springframework.boot.loader.JarLauncher"
--- src/main/kotlin/com/joshlong/twitter/TwitterApplication.kt
+++ src/main/kotlin/com/joshlong/twitter/TwitterApplication.kt
@@ -1,8 +1,18 @@
 package com.joshlong.twitter
 
+import com.joshlong.twitter.integration.RedisMetadataStore
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.integration.context.IntegrationContextUtils
 
 @SpringBootApplication
 @EnableConfigurationProperties(IngestTwitterProperties::class)
-class TwitterApplication
+class TwitterApplication {
+
+	@Bean(IntegrationContextUtils.METADATA_STORE_BEAN_NAME)
+	fun redisMetadataStore(stringRedisTemplate: StringRedisTemplate) =
+			RedisMetadataStore(stringRedisTemplate)
+
+}
